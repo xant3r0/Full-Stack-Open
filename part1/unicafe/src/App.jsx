698fbeb0754e9d9d32677fdf13286addb84011db
xant3r0 <scandiba13@gmail.com>
2026-08-27 17:05:20 +0300

    Completed ex 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,12 +4,30 @@ const Button = ({text,onClick}) => <button onClick={onClick}>{text}</button>
 
 const StatisticDisplay = ({title,counter,children}) => <p>{title} {counter} {children}</p>
 
+const Statistics = ({counters}) => {
+
+  const [good,neutral,bad,total] = counters;
+  const avg = (good - bad) / total;
+  const pos = (good / total) * 100;
+
+  return (
+      <div>
+        <StatisticDisplay title={'Good'} counter={good}></StatisticDisplay>
+        <StatisticDisplay title={'Neutral'} counter={neutral}></StatisticDisplay>
+        <StatisticDisplay title={'Bad'} counter={bad}></StatisticDisplay>
+        <StatisticDisplay title={'All'} counter={total}></StatisticDisplay>
+        <StatisticDisplay title={'Average'} counter={avg}></StatisticDisplay>
+        <StatisticDisplay title={'Positive'} counter={pos}>%</StatisticDisplay>
+      </div>
+  )
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  let total = good + neutral + bad;
+  const total = good + neutral + bad;
 
   return (
       <div>
@@ -18,12 +36,7 @@ const App = () => {
         <Button text={'Neutral'} onClick={() => setNeutral(neutral + 1)}></Button>
         <Button text={'Bad'} onClick={() => setBad(bad + 1)}></Button>
         <h1>Statistics</h1>
-        <StatisticDisplay title={'Good'} counter={good}></StatisticDisplay>
-        <StatisticDisplay title={'Neutral'} counter={neutral}></StatisticDisplay>
-        <StatisticDisplay title={'Bad'} counter={bad}></StatisticDisplay>
-        <StatisticDisplay title={'All'} counter={total}></StatisticDisplay>
-        <StatisticDisplay title={'Average'} counter={(good - bad) / total}></StatisticDisplay>
-        <StatisticDisplay title={'Positive'} counter={(good / total) * 100}>%</StatisticDisplay>
+        <Statistics counters={[good,neutral,bad,total]}></Statistics>
       </div>
   )
 };
