f0b02e600cd5efe25ff5efff61bbb9bad54f6ce3
xant3r0 <scandiba13@gmail.com>
2026-08-27 16:48:49 +0300

    Completed ex 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const Button = ({text,onClick}) => <button onClick={onClick}>{text}</button>
 
-const StatisticDisplay = ({title,counter}) => <p>{title} {counter}</p>
+const StatisticDisplay = ({title,counter,children}) => <p>{title} {counter} {children}</p>
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  let total = good + neutral + bad;
 
   return (
       <div>
@@ -20,6 +21,9 @@ const App = () => {
         <StatisticDisplay title={'Good'} counter={good}></StatisticDisplay>
         <StatisticDisplay title={'Neutral'} counter={neutral}></StatisticDisplay>
         <StatisticDisplay title={'Bad'} counter={bad}></StatisticDisplay>
+        <StatisticDisplay title={'All'} counter={total}></StatisticDisplay>
+        <StatisticDisplay title={'Average'} counter={(good - bad) / total}></StatisticDisplay>
+        <StatisticDisplay title={'Positive'} counter={(good / total) * 100}>%</StatisticDisplay>
       </div>
   )
 };
