502a7927898b390ebcf45e023784c30bee61d5dc
xant3r0 <scandiba13@gmail.com>
2026-08-30 18:31:05 +0300

    Completed ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from "./components/Filter.jsx";
 import PersonForm from "./components/PersonForm.jsx";
 import Persons from "./components/Persons.jsx";
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const getPersonsFromServer = () => {
+      axios
+          .get('http://localhost:3001/persons')
+          .then(res => setPersons(res.data))
+  }
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName,setNewFilter] = useState('')
-
   const filterHandler = (event) => {
       setNewFilter(event.target.value)
   }
@@ -40,6 +40,8 @@ const App = () => {
        setNewNumber('')
      }
   }
+
+  useEffect(getPersonsFromServer, []);
 
   return (
       <div>
