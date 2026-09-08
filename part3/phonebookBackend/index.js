e786e259f35d1b034dbae304fcd58a9b062b4858
xant3r0 <scandiba13@gmail.com>
2026-09-08 19:03:55 +0300

    Completed ex 3.22

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -8,91 +8,91 @@ const app = express()
 const PORT = process.env.PORT
 
 morgan.token('person',(req) => {
-    if(req.method === 'POST') {
-        const {name,number} = req.body
-        const person = {"name":name,"number":number}
-        return JSON.stringify(person)
-    }
+  if(req.method === 'POST') {
+    const { name,number } = req.body
+    const person = { 'name':name,'number':number }
+    return JSON.stringify(person)
+  }
 })
 
 app.use(express.json()).use(morgan((tokens,req,res) => {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        tokens['person'](req, res)
-    ].join(' ')
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens['person'](req, res)
+  ].join(' ')
 })).use(cors()).use(express.static('dist'))
 
 app.get('/api/persons',(req,res,next) => {
-    People.find({}).then(data => {
-        res.status(200).json(data).end()
+  People.find({}).then(data => {
+    res.status(200).json(data).end()
 
-    }).catch(error => next(error))
+  }).catch(error => next(error))
 })
 
 app.get('/info',(req,res, next) => {
-    const date = new Date()
-    People.collection.countDocuments().then(count => res.send(`<p>Phonebook has info for ${count} people</p><p>${date.toString()}</p>`))
-        .catch(e => next(e))
+  const date = new Date()
+  People.collection.countDocuments().then(count => res.send(`<p>Phonebook has info for ${count} people</p><p>${date.toString()}</p>`))
+    .catch(e => next(e))
 })
 
 app.get('/api/persons/:id',(req,res,next) => {
-    const id = req.params.id
-    People.findById(id).then(data => {
-        //return res.status(200).json(data)
-        return data === null ? res.status(404).json({error:"There isn't such a user with the provided Id!"}) : res.status(200).json(data)
-    })//.catch(error => next(error))
+  const id = req.params.id
+  People.findById(id).then(data => {
+    //return res.status(200).json(data)
+    return data === null ? res.status(404).json({ error:'There isn\'t such a user with the provided Id!' }) : res.status(200).json(data)
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id',(req,res,next) => {
-    const id = req.params.id
-    People.findByIdAndDelete(id)
-        .then(data => data === null ? res.status(404).json({error:"This note doesnt exist or was already deleted!"}) : res.status(204).end())
-        .catch(error => next(error))
+  const id = req.params.id
+  People.findByIdAndDelete(id)
+    .then(data => data === null ? res.status(404).json({ error:'This note doesnt exist or was already deleted!' }) : res.status(204).end())
+    .catch(error => next(error))
 })
 
-app.post('/api/persons',(req,res,next) => {
-    const {name,number} = req.body
+app.post('/api/persons',(req,res) => {
+  const { name,number } = req.body
 
-    if(!name.length || number === undefined) {
-        return res.status(400).json({error:"The name or number is missing!"})
-    }
+  if(!name.length || number === undefined) {
+    return res.status(400).json({ error:'The name or number is missing!' })
+  }
 
-    const person = new People({
-        name:name,
-        number:number
+  const person = new People({
+    name:name,
+    number:number
+  })
+
+  person.save()
+    .then(() => res.status(201).json({ message:'Person successfully added!',data:person }).end())
+    .catch(error => {
+      //console.log(error)
+      res.status(400).json(error)
+      // if(error._message === 'person validation failed') {
+      //    res.status(400).json({error:"Person validation failed!"})
+      // }
+      //next(error)
     })
-
-    person.save()
-        .then(() => res.status(201).json({message:"Person successfully added!",data:person}).end())
-        .catch(error => {
-            //console.log(error)
-            res.status(400).json(error)
-            // if(error._message === 'person validation failed') {
-            //    res.status(400).json({error:"Person validation failed!"})
-            // }
-            //next(error)
-        })
 })
 
-app.put('/api/persons/:id',(req, res, next) => {
-    const id = req.params.id
-    const newNumber = req.body.number
+app.put('/api/persons/:id',(req, res) => {
+  const id = req.params.id
+  const newNumber = req.body.number
 
-    People.findByIdAndUpdate(
-        id,
-        {number:newNumber},
-        { returnDocument:'after', runValidators: true }
-    ).then(data => {
-        data === null ? res.status(404).json("A note with such Id doesn't exist!") : res.status(200).json(data)
-    }).catch(error => res.status(400).json(error))
+  People.findByIdAndUpdate(
+    id,
+    { number:newNumber },
+    { returnDocument:'after', runValidators: true }
+  ).then(data => {
+    data === null ? res.status(404).json('A note with such Id doesn\'t exist!') : res.status(200).json(data)
+  }).catch(error => res.status(400).json(error))
 })
 
 app.use(error)
 
 app.listen(PORT,() => {
-    console.log(`Server started on port ${PORT}`)
+  console.log(`Server started on port ${PORT}`)
 })
--- part3/phonebookBackend/modules/People.js
+++ part3/phonebookBackend/modules/People.js
@@ -1,36 +1,36 @@
 const mongoose = require('mongoose')
 const path = require('path')
 const dns = require('node:dns')
-require('dotenv').config({path:path.resolve('modules','../.env')})
+require('dotenv').config({ path:path.resolve('modules','../.env') })
 
 dns.setServers(['8.8.8.8'])
 
 const url = process.env.MONGO_DB_URL
 mongoose.set('strictQuery',false)
 
-mongoose.connect(url, {family: 4}).then(() => console.log("Connected to server!"))
+mongoose.connect(url, { family: 4 }).then(() => console.log('Connected to server!'))
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type:String,
-        validate: {
-            validator: n => /^(?:\d{2}-\d{6,}|\d{3}-\d{5,})$/.test(n),
-            message: props => `${props.value} is not a valid phone number!`
-        }
+  name: {
+    type:String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type:String,
+    validate: {
+      validator: n => /^(?:\d{2}-\d{6,}|\d{3}-\d{5,})$/.test(n),
+      message: props => `${props.value} is not a valid phone number!`
     }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = new mongoose.model('person',personSchema)
